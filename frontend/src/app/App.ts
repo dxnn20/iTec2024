9b01fd77951ae59f3c373af690742f4635dc5802
Daniel <turutadaniel20@gmail.com>
2024-04-05 23:04:22 +0300

    IDEK ATP dar merge

--- frontend/src/app/App.ts
+++ frontend/src/app/App.ts
@@ -1,8 +1,13 @@
+import {Endpoint} from "./security/endpoint";
+
 export class App {
   name: string;
   status: string;
+  endpoint: Endpoint;
 
-  constructor(name: string, status: string) {
+  constructor(name: string, status: string, endpoint: Endpoint) {
     this.name = name;
+    this.status = status;
+    this.endpoint = endpoint;
   }
 }
