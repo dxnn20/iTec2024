16d2dcbd83d8396d8d8d05828ac350d562098d3a
Daniel <turutadaniel20@gmail.com>
2024-04-05 23:04:22 +0300

    IDEK ATP dar merge

--- frontend/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ frontend/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-page',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit{
 
+
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+  }
+
+  ngOnInit()
+  {
+    console.log(this.route.snapshot.params['id']);
+    this.http.get("http://localhost:1201/app/getall").subscribe(
+      (data: any)=>{
+        if(data != null)
+        console.log(data);
+      }
+    )
+  }
 }
