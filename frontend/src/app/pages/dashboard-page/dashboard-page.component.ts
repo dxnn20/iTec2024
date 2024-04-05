fdd5a2c952b587ec50d1f246bc6b3553dfb2a652
Daniel <turutadaniel20@gmail.com>
2024-04-05 23:30:48 +0300

    fixing loading data into dashboard

--- frontend/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ frontend/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
+import {App} from "../../App";
+import {SecurityService} from "../../security/security.service";
+
+let apps: App[];
 
 @Component({
   selector: 'app-dashboard-page',
@@ -9,20 +13,20 @@ import {ActivatedRoute} from "@angular/router";
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
-export class DashboardPageComponent implements OnInit{
+export class DashboardPageComponent implements OnInit {
 
 
-
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
+  constructor(private http: HttpClient, private route: ActivatedRoute,private securityService: SecurityService) {
   }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     console.log(this.route.snapshot.params['id']);
-    this.http.get("http://localhost:1201/app/getall").subscribe(
-      (data: any)=>{
-        if(data != null)
-        console.log(data);
+    this.http.get("http://localhost:1201/app/getAllByUserId/" + this.securityService.getId()).subscribe(
+      (data: any) => {
+        if (data != null)
+          console.log(data)
+        else
+          apps = data
       }
     )
   }
--- frontend/src/app/security/User.ts
+++ frontend/src/app/security/User.ts
@@ -1,6 +1,5 @@
 export class User{
-  id: number | undefined;
-  username: string | undefined;
-  password: string | undefined;
-  role: string | undefined;
+  id: number;
+  username: string;
+  password: string;
 }
