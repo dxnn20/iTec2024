9fbd9bb1b5fa0f4d95807df3067f2e346d213a64
Daniel <turutadaniel20@gmail.com>
2024-04-07 02:04:40 +0300

    AVEM AUTO REFRESH

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -17,6 +17,7 @@ import {App} from "../../App";
 import {Router} from "@angular/router";
 import {MatButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
+import {interval} from "rxjs";
 
 export interface PeriodicElement {
   name: string;
@@ -62,7 +63,9 @@ export class HomePageComponent {
   }
 
   ngOnInit() {
-    this.refresh()
+    interval(1000).subscribe(() => {
+      this.refresh()
+    })
   }
 
   refresh() {
@@ -73,14 +76,12 @@ export class HomePageComponent {
         for (let i = 0; i < data.length; i++)
           if (data[i].status == null)
             data[i].status = "DOWN"
-
-        console.log(data)
+        
       }
     )
   }
 
   onClick(app: App) {
-    console.log(app)
     this.router.navigateByUrl('/view-endpoints/' + app.id).then(r => console.log(r))
   }
 
